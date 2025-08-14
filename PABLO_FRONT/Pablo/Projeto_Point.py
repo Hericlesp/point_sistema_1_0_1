e3a73184024877bd0b5b9aad79109dbb244d52b0
HERICLES MENDES <hericlespaulo05@gmail.com>
2025-08-13 21:55:03 -0300

    Update Projeto_Point.py

--- PABLO_FRONT/Pablo/Projeto_Point.py
+++ PABLO_FRONT/Pablo/Projeto_Point.py
@@ -6,8 +6,8 @@ class Point:
     def __init__(self,janela):
         self.janela=janela
         self.janela.title("Area do Aluno")
-        self.janela.geometry("300x450")
-        self.janela.tk_setPalette(background="green")
+        self.janela.geometry("450x350")
+        self.janela.tk_setPalette(background="#E2E2E2")
         self.tarefas = []
 
         # Configuração do tema
@@ -16,69 +16,74 @@ class Point:
         self.style.theme_use("clam")
 
         self.criar_widgets()
-        self.run()
 
     def criar_widgets(self):
         
         
         #Frame de Entrada
+    
 
-        up_frame = tk.Frame(self.janela)
-        up_frame.pack()
+
+        up_frame = tk.Frame(self.janela, background="#002CA7")
+        up_frame.pack(fill="x", pady=15)
+        
+        up_frame_config = tk.Frame(up_frame, background="#002CA7")
+        up_frame_config.grid(column=0, padx=15)
 
         #Frame Principal
 
-        frame_principal = tk.Frame(self.janela)
+        frame_principal = tk.Frame(self.janela, background="#E2E2E2")
         frame_principal.pack()
 
-        down_frame = tk.Frame(self.janela)
+        down_frame = tk.Frame(self.janela, background="#002CA7")
         down_frame.pack()
 
 
         
         # Labels Nome do Aluno e Descrição do Curso
-        self.descricao_curso = tk.Label(up_frame, text="Tecnico de Internet", font=("Arial", 16, "bold"), bg="#690E0E", fg="white")
-        self.descricao_curso.grid(row=0, column=1, padx=50, pady=5)
-
-        self.nome_aluno = tk.Label(up_frame, text="Nome do Aluno", font=("Arial", 14), bg="#690E0E", fg="white")
-        self.nome_aluno.grid(row=1, column=1, padx=50)
         
-        self.matricula = tk.Label(up_frame,text=(f"MATRICULA:{"matricula"}"), font=("Arial", 10), bg="#690E0E", fg="white")
-        self.matricula.grid(row=2, column=1, padx=50)
+        
+    
+        self.descricao_curso = tk.Label(up_frame, text="Tecnico de Internet", font=("Arial", 22, "bold"), bg="#002CA7",fg="#e2e2e2")
+        self.descricao_curso.grid(row=0, column=1, pady=15)
+
+        self.nome_aluno = tk.Label(up_frame, text="Nome do Aluno", font=("Arial", 18), bg="#002CA7", fg="#e2e2e2")
+        self.nome_aluno.grid(row=2, column=1, padx=50)
+        
+        self.matricula = tk.Label(up_frame,text=(f"MATRICULA:{"matricula"}"), font=("Arial", 14), bg="#002CA7", fg="#e2e2e2")
+        self.matricula.grid(row=3, column=1, padx=50)
         
 
 
 
         #Labels e Botões para Marcar Frequência
         
-        self.entrada_frequencia = tk.Label(frame_principal, text="Primeiro horário", font=("Arial", 12), bg="#690E0E", fg="white")
-        self.entrada_frequencia.grid(row=2, column=1, padx=50, pady=20)
-        self.entrada_frequencia = tk.Button(frame_principal,text="Marcar Frequência", width=20)
+        self.entrada_horario = tk.Button(up_frame_config,text="⚙️", width=2, font=("Arial", 14), fg="#e2e2e2", bg="#002CA7")
+        self.entrada_horario.grid(row=0, column=0, padx=20, pady=5)
+
+        self.entrada_horario = tk.Label(frame_principal,text="Horário", font=("Arial", 14), width=20)
+        self.entrada_horario.grid(row=0, column=1, padx=50, pady=5)
+                
+        self.entrada_frequencia = tk.Button(frame_principal,text="Marcar Frequência", font=("Arial", 14),background="#002CA7",fg="white", width=20)
         self.entrada_frequencia.grid(row=3, column=1, padx=50, pady=5)
         self.entrada_frequencia.focus()
+        
+        self.entrada_horario = tk.Entry(frame_principal, background="white", width=40) 
+        self.entrada_horario.grid(row=1, column=1, padx=50, pady=5)
+        
 
-        self.entrada_frequencia = tk.Label(frame_principal, text="Segundo horário", font=("Arial", 12), bg="#690E0E", fg="white")
-        self.entrada_frequencia.grid(row=4, column=1, padx=80, pady=20)
-        self.entrada_frequencia2= tk.Button(frame_principal, text="Marcar Frequência", width=20)
-        self.entrada_frequencia2.grid(row=5, column=1, padx=80, pady=5)
+       
+        
 
-        self.entrada_frequencia = tk.Label(frame_principal, text="Terceiro horário", font=("Arial", 12), bg="#690E0E", fg="white")
-        self.entrada_frequencia.grid(row=6, column=1, padx=80, pady=20)
-        self.entrada_frequencia3= tk.Button(frame_principal, text="Marcar Frequência", width=20)
-        self.entrada_frequencia3.grid(row=7, column=1, padx=80, pady=5)
-
-    def run(self):
-        self.janela.mainloop()
         
 
        
 
 
-if __name__ == "__main__":
 
-    janela= tk.Tk()
-    app = Point(janela)
-    janela.mainloop()
+janela= tk.Tk()
+app = Point(janela)
+janela.mainloop()
 
 
     
