88fc9a001a5d114e809837d73783a4f07bdcdfb9
HERICLES MENDES <hericlespaulo05@gmail.com>
2025-08-13 21:16:26 -0300

    Update LOGIN101.py

--- PABLO_FRONT/Erick/LOGIN101.py
+++ PABLO_FRONT/Erick/LOGIN101.py
@@ -1,8 +1,25 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+import sqlite3
+import os
+
+import sys
+sys.path.append(r"\\educcur03\Users\Public\Técnico Informática Noite\point_sistema_1_0_1")
+
+from PABLO_FRONT.Pablo.Projeto_Point import Point
+import PABLO_FRONT.Deyu.adm_aparencer_professor as adm
+
 #import Pagina_de_Funcoes as Pf
 
+#CAMINHO DO BANCO
+
+
+DB_DIR = r"\\educcur03\Users\Public\Técnico Informática Noite\point_sistema_1_0_1\CASSIA_BACK\Henrique\data"
+DB_PATH = os.path.join(DB_DIR, 'database.db')
+
+sys.path.append(r"\\educcur03\Users\Public\Técnico Informática Noite\point_sistema_1_0_1")
 
 
 class POINT:
@@ -20,6 +37,9 @@ class POINT:
     #self.style.configure(bg="l")
     # self.style.theme_use("")
     self.criar_widgets()
+    
+    
+    
 
 
   def criar_widgets(self):
@@ -54,9 +74,9 @@ class POINT:
     
     self.entrada_senha = tk.Entry(frame_principal, textvariable=self.var_senha, show="*", font=("arial", 16), width=50)
     self.entrada_senha.pack( padx=5, pady=5)
-    self.entrada_senha.focus()
+    
 
-    botao_login = tk.Button(frame_principal, text="LOGIN", font=("arial",16,"bold") ,width=20,)
+    botao_login = tk.Button(frame_principal, text="LOGIN", font=("arial",16,"bold") ,width=20,command=self.fazer_login)
     botao_login.pack(padx=2,pady=5)
     
       # _____________ FOOTER_____________        
@@ -66,10 +86,60 @@ class POINT:
     footer2 =tk.Label(frame_donw, text=("TEC. INTERNET - SERVENTE D' SOFTWARE"),fg="white", bg="#002CA7",font=("arial",10,"italic"))
     footer2.pack( padx=10)  
     
-    def fazer_login():
-      pass
+  def fazer_login(self):
+    usuario =self.var_usuario.get().strip().upper()
+    senha = self.var_senha.get().strip().upper()
+
+    #validação inicial    
+    if not usuario or not senha:
+      messagebox.showwarning("ATENÇÃO"," Preencha todos os campos")
+      return
+    
+    try:
+      usuario_int = int(usuario)
       
-      us
+    except ValueError:
+      messagebox.showerror("ERRO"," Usuario ou senha incorreto")
+      return
+    
+    
+    #conecta ao banco para extrair os dados
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+  
+    cursor.execute(
+    "SELECT NOME,PERMISSAO FROM login WHERE (CPF = ? OR RA = ?) AND SENHA = ? ",(usuario_int,usuario_int, senha)
+    )
+  
+    resultado = cursor.fetchone()
+    conn.commit()
+
+
+    #valida entrada
+    if resultado:
+      
+      user = resultado[1]
+      user_name = resultado[0]
+      
+      messagebox.showinfo("Login",f"Bem-Vindo, {user_name.upper()}!")
+      
+      self.janela.destroy()
+      
+      if user in ("ROOT", "PROFESSOR"):
+        new_janela = tk.Tk()
+        Point(new_janela).run()
+        
+      elif user == "aluno":
+        new_janela = tk.Tk()
+        adm.Inicial(new_janela,usuario_int).run()
+      
+      else:
+        messagebox.showerror("ERRO", " Permissão desconhecida")
+    
+    else:
+      messagebox.showerror("ERRO!"," Matrícula ou senha incorreos.")
+      
+    
 
 
 if __name__== "__main__":
